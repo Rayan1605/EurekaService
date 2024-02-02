cd4667a588df029177e7f1c045afb5b5c10a73a6
rayan <rayanzyad99@gmail.com>
2024-02-02 03:07:58 +0000

    Start

---
+++ src/test/java/com/example/photodiscoveryservice/PhotoDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photodiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
