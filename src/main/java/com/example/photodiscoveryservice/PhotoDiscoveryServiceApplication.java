a701ba63ca015eb15bc943b38e1b1d9e9faa84bb
rayan <rayanzyad99@gmail.com>
2024-02-02 03:13:12 +0000

    Start

--- src/main/java/com/example/photodiscoveryservice/PhotoDiscoveryServiceApplication.java
+++ src/main/java/com/example/photodiscoveryservice/PhotoDiscoveryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer // This annotation make your spring boot application act as a Eureka server
 public class PhotoDiscoveryServiceApplication {
 
     public static void main(String[] args) {
